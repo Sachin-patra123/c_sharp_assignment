e436779de184a6e00399a9ff671df03cc436ec09
Sachin-patra123 <sachin.patra@capgemini.com>
2022-04-24 20:47:26 +0530

    Added assignment 1

---
+++ Assignment1/Circle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    public class Circle
+    {
+        static void circle(int r)
+        {
+            
+            Console.WriteLine("Area = " + Math.PI * Math.Pow(r, 2));
+            Console.WriteLine("Circumference = " + 2 * Math.PI * r);
+        }
+        static void Main(string[] args)
+        {
+            int r = 3;
+            circle(r);
+            //Circle.circle(r);
+            Console.ReadLine();
+        }
+    }
+}
